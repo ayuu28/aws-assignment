fc4cc3dc39d174f3ab7670729544c7dd4f3be831
ayuu28 <115541002+ayuu28@users.noreply.github.com>
2025-05-19 00:11:58 +0530

    Add files via upload
    Uploaded Task 2 Terraform codes.

---
+++ task2/main.tf
@@ -0,0 +1,117 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = var.private_subnet_cidr
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  name        = "private-sg"
+  description = "Allow SSH from Bastion"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami           = "ami-03bb6d83c60fc5f7c" # Amazon Linux 2023 AMI (ap-south-1)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  key_name      = var.key_name
+  security_groups = [aws_security_group.bastion_sg.name]
+  tags = {
+    Name = "BastionHost"
+  }
+}
+
+resource "aws_instance" "private_ec2" {
+  ami           = "ami-03bb6d83c60fc5f7c"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_subnet.id
+  key_name      = var.key_name
+  security_groups = [aws_security_group.private_sg.name]
+  tags = {
+    Name = "PrivateInstance"
+  }
+}
